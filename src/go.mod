b851d88afb12e107cc96cd1344e46a80674b5229
Guido Westenberg <gwestenberg@pivotal.io>
2020-12-04 08:31:04 -0600

    Create websocket endpoint

--- src/go.mod
+++ src/go.mod
@@ -3,3 +3,8 @@ module main
 go 1.14
 
 replace lightshow => ./lightshow
+
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/gorilla/websocket v1.4.2
+)
--- src/lightshow/api.go
+++ src/lightshow/api.go
@@ -2,9 +2,11 @@ package lightshow
 
 import (
 	"io"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
 
 // API interface
@@ -13,8 +15,9 @@ type API interface {
 }
 
 type api struct {
-	router  *mux.Router
-	pattern string
+	router   *mux.Router
+	upgrader websocket.Upgrader
+	pattern  string
 }
 
 // NewAPI returns an initialized Client API context
@@ -22,9 +25,11 @@ func NewAPI() API {
 	router := mux.NewRouter().StrictSlash(true)
 	api := api{
 		router,
+		websocket.Upgrader{},
 		"solid #000",
 	}
 	router.HandleFunc("/pattern", api.getPattern)
+	router.HandleFunc("/connect", api.connect)
 	return api
 }
 
@@ -35,4 +40,49 @@ func (context api) ServeHTTP(response http.ResponseWriter, request *http.Request
 func (context *api) getPattern(response http.ResponseWriter, request *http.Request) {
 	response.WriteHeader(http.StatusOK)
 	io.WriteString(response, context.pattern)
+}
+
+func (context *api) connect(response http.ResponseWriter, request *http.Request) {
+
+	connection, err := context.upgrader.Upgrade(response, request, nil)
+	if err != nil {
+		log.Print("Upgrade error:", err)
+		http.Error(response, "Upgrade Error", http.StatusBadRequest)
+		return
+	}
+	defer connection.Close()
+
+	done := make(chan struct{})
+	go context.sendUpdates(connection, done)
+	context.handleRequests(connection)
+	close(done)
+}
+
+func (context *api) handleRequests(connection *websocket.Conn) {
+	for {
+		var message string
+		err := connection.ReadJSON(&message)
+		if err != nil {
+			if !websocket.IsCloseError(err, websocket.CloseNormalClosure) {
+				log.Printf("Websocket error: %v", err)
+			}
+			return
+		}
+	}
+}
+
+func (context *api) sendUpdates(connection *websocket.Conn, done chan struct{}) {
+
+	for { // Outer loop repeats when device pairing changes
+		select {
+		// case deviceUpdate := <-deviceChannel:
+		// 	connection.WriteJSON(deviceUpdate)
+		// 	break
+		// case clientUpdate := <-clientChannel:
+		// 	pairedDeviceID = clientUpdate.Get("pairedDeviceID").(string)
+		// 	break
+		case <-done:
+			return
+		}
+	}
 }
