90ecb8367cbbc6ba16618d9319bba6c1e67aa026
Guido Westenberg <guidowb@gmail.com>
2021-12-13 20:21:43 -0600

    deployment moved to mono repo

--- deployment/gcr-secret.sh
+++
@@ -1,5 +0,0 @@
-kubectl create secret docker-registry gcr-registry-credentials \
-  --docker-server=https://us.gcr.io \
-  --docker-username=_json_key \
-  --docker-email=user@example.com \
-  --docker-password="$(cat ~/.secrets/k8s-gcr-auth-rw.json)"
